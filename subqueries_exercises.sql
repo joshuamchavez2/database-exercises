3f8c9b84b13608804838de3381b5311734cfc9c4
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-06-30 21:09:56 -0500

    finished exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -36,14 +36,106 @@ WHERE emp_no in(
 	WHERE dept_emp.to_date < now()
 	);
 
-/* Find all the current department managers that are female. List their names in a comment in your code. */
+/* Find all the current department managers that are female. List their names in a comment in your code. 
+Isamu	Legleitner
+Karsten	Sigstam
+Leon	DasSarma
+Hilary	Kambil*/
 
-SELECT 
+SELECT first_name, last_name
 FROM employees
-WHERE 
+WHERE gender 
+	IN(
+	SELECT gender
+	FROM employees
+    WHERE gender = 'F'
+	) 
+    AND emp_no 
+      IN(SELECT emp_no
+      from dept_manager
+      where to_date > now()
+      );
 
 /* Find all the employees who currently have a higher salary than the companies overall, historical average salary. */
 
-
-
+SELECT last_name, first_name
+FROM employees
+WHERE emp_no IN(
+	SELECT emp_no
+    FROM salaries
+      WHERE salary > 
+      		(select 
+      		avg(salary) 
+      		from salaries
+      		) AND salaries.to_date > now()
+      	);
 /* How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this? */
+
+select count(*)
+from employees
+where emp_no in(
+select emp_no
+from salaries
+where salaries.to_date > now() AND 
+
+salary between (
+			select salary
+			from salaries
+			where salaries.to_date > now() and emp_no in (
+				SELECT emp_no
+				FROM employees
+				WHERE emp_no IN(
+					SELECT emp_no
+    				FROM salaries
+      				WHERE salary = 
+      					(select 
+      					max(salary) 
+      					from salaries
+      					) AND salaries.to_date > now()
+      				)
+      			)
+      			)-(
+				select STD(salary)
+				from salaries
+				where salaries.to_date > now()) 
+				
+			AND 
+				
+				
+				   (select salary
+					from salaries
+					where salaries.to_date > now() and emp_no in (
+						SELECT emp_no
+						FROM employees
+						WHERE emp_no IN(
+							SELECT emp_no
+    						FROM salaries
+      						WHERE salary = 
+      							(select max(salary) 
+      							from salaries
+      							) AND salaries.to_date > now()
+      						)
+      					)
+      				)
+);
+			
+/* select STD(salary)
+from salaries
+where salaries.to_date > now();
+
+
+select salary
+from salaries
+where salaries.to_date > now() and emp_no in (
+	SELECT emp_no
+	FROM employees
+	WHERE emp_no IN(
+		SELECT emp_no
+    	FROM salaries
+      	WHERE salary = 
+      			(select 
+      			max(salary) 
+      			from salaries
+      			) AND salaries.to_date > now()
+      	)
+      	); */
