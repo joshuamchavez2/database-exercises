ee851ce166f0081bd3f57e18e6ef3530bbb02268
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-07-04 21:26:38 -0500

    reviewed where exercises and adjusted code for readability

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,6 @@
 
 
-
+USE employees;
 DESCRIBE employees;
 
 
@@ -12,37 +12,36 @@ WHERE first_name IN('Irena', 'Vidya', 'Maya');
 
 SELECT *
 FROM employees
-WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+WHERE	first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
 #709 records returned
 
 SELECT *
 FROM employees
 
-WHERE 
-(first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
-AND
-(gender = 'M');
+WHERE 	(first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+	AND
+		(gender = 'M');
 #441 records returned
 
 SELECT *
 FROM employees
-WHERE last_name like 'E%';
+WHERE last_name LIKE 'E%';
 
 #7330 records found
 
 SELECT *
 FROM employees
-WHERE (last_name not like 'E%')
-AND
-(last_name like '%E');
+WHERE 	(last_name NOT LIKE'E%')
+	AND
+		(last_name LIKE '%E');
 #How many employees have a last name that ends with E, but does not start with E = 23393
 
 SELECT *
 FROM employees
-WHERE (last_name like 'E%')
-or
-(last_name like '%E');
+WHERE 	(last_name LIKE 'E%')
+	OR
+		(last_name LIKE '%E');
 #Number of employees whose last name starts or ends with E = 30723
 
 /* 
@@ -52,13 +51,13 @@ How many employees' last names end with E, regardless of whether they start with
 
 SELECT *
 FROM employees
-WHERE (last_name like 'E%') AND (last_name like '%E');
+WHERE (last_name LIKE 'E%') AND (last_name LIKE '%E');
 
 # Enter a comment with the number of employees whose last name starts and ends with E = 899
 
 SELECT *
 FROM employees
-WHERE (last_name like '%E');
+WHERE last_name LIKE '%E';
 
 # How many employees' last names end with E, regardless of whether they start with E = 24292
 
@@ -76,21 +75,23 @@ WHERE birth_date LIKE '%12-25';
 
 SELECT *
 FROM employees
-WHERE (hire_date LIKE '199%')
-AND
-(birth_date LIKE '%12-25');
+WHERE 	(hire_date LIKE '199%')
+	AND
+		(birth_date LIKE '%12-25');
 
 #362 employees born on christmas and hired in the 90s
 
 SELECT *
 FROM employees
-WHERE (last_name like '%q%');
+WHERE last_name LIKE '%q%';
 
 #1873 employees have the letter q in their last name.
 
 SELECT *
 FROM employees
-WHERE (last_name like '%q%') and (last_name not like '%qu%');
+WHERE 	(last_name LIKE '%q%') 
+	AND
+		(last_name NOT LIKE '%qu%');
 
 #547 employees have q but not qu in last name
 
